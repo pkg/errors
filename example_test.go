b402999421fbf343bfc95f38ba67fc248620b598
Dave Cheney <dave@cheney.net>
2016-04-22 17:40:16 +0900

    more examples

--- example_test.go
+++ example_test.go
@@ -1,13 +1,35 @@
 package errors_test
 
 import (
-	"errors"
 	"fmt"
+
+	"github.com/pkg/errors"
 )
 
 func ExampleNew() {
 	err := errors.New("whoops")
-	fmt.Println(err.Error())
+	fmt.Println(err)
 
 	// Output: whoops
+}
+
+func ExampleWrap() {
+	cause := errors.New("whoops")
+	err := errors.Wrap(cause, "oh noes")
+	fmt.Println(err)
+
+	// Output: oh noes: whoops
+}
+
+func fn() error {
+	return errors.Wrap(errors.Wrap(errors.Wrap(errors.New("error"), "inner"), "middle"), "outer")
+}
+
+func ExampleCause() {
+	err := fn()
+	fmt.Println(err)
+	fmt.Println(errors.Cause(err))
+
+	// Output: outer: middle: inner: error
+	// error
 }
