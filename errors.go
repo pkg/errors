96334e6f662e203c41613c316957f34b6e126eec
Dave Cheney <dave@cheney.net>
2016-06-06 23:01:57 +1000

    wip

--- errors.go
+++ errors.go
@@ -21,15 +21,6 @@
 //              return errors.Wrap(err, "read failed")
 //      }
 //
-// Retrieving the stack trace of an error or wrapper
-//
-// New, Errorf, Wrap, and Wrapf record a stack trace at the point they are
-// invoked. This information can be retrieved with the following interface.
-//
-//     type Stack interface {
-//             Stack() []uintptr
-//     }
-//
 // Retrieving the cause of an error
 //
 // Using errors.Wrap constructs a stack of errors, adding context to the
@@ -51,24 +42,22 @@
 //     default:
 //             // unknown error
 //     }
+//
+// Retrieving the stack trace of an error or wrapper
+//
+// New, Errorf, Wrap, and Wrapf record a stack trace at the point they are
+// invoked. This information can be retrieved with the following interface.
+//
+//     type Stacktrace interface {
+//             Stacktrace() []Frame
+//     }
 package errors
 
 import (
 	"errors"
 	"fmt"
 	"io"
-	"runtime"
-	"strings"
 )
-
-// stack represents a stack of program counters.
-type stack []uintptr
-
-func (s *stack) Stack() []uintptr { return *s }
-
-func (s *stack) Location() (string, int) {
-	return location((*s)[0] - 1)
-}
 
 // New returns an error that formats as the given text.
 func New(text string) error {
@@ -167,11 +156,16 @@ func Cause(err error) error {
 // Fprint prints the error to the supplied writer.
 // If the error implements the Causer interface described in Cause
 // Print will recurse into the error's cause.
-// If the error implements the inteface:
+// If the error implements one of the following interfaces:
+//
+//     type Stacktrace interface {
+//            Stacktrace() []Frame
+//     }
 //
 //     type Location interface {
 //            Location() (file string, line int)
 //     }
+//
 //
 // Print will also print the file and line of the error.
 // If err is nil, nothing is printed.
@@ -179,14 +173,23 @@ func Fprint(w io.Writer, err error) {
 	type location interface {
 		Location() (string, int)
 	}
+	type stacktrace interface {
+		Stacktrace() []Frame
+	}
 	type message interface {
 		Message() string
 	}
 
 	for err != nil {
-		if err, ok := err.(location); ok {
+		switch err := err.(type) {
+		case stacktrace:
+			frame := err.Stacktrace()[0]
+			fmt.Fprintf(w, "%+s:%d: ", frame, frame)
+		case location:
 			file, line := err.Location()
 			fmt.Fprintf(w, "%s:%d: ", file, line)
+		default:
+			// de nada
 		}
 		switch err := err.(type) {
 		case message:
@@ -201,59 +204,4 @@ func Fprint(w io.Writer, err error) {
 		}
 		err = cause.Cause()
 	}
-}
-
-func callers() *stack {
-	const depth = 32
-	var pcs [depth]uintptr
-	n := runtime.Callers(3, pcs[:])
-	var st stack = pcs[0:n]
-	return &st
-}
-
-// location returns the source file and line matching pc.
-func location(pc uintptr) (string, int) {
-	fn := runtime.FuncForPC(pc)
-	if fn == nil {
-		return "unknown", 0
-	}
-
-	// Here we want to get the source file path relative to the compile time
-	// GOPATH. As of Go 1.6.x there is no direct way to know the compiled
-	// GOPATH at runtime, but we can infer the number of path segments in the
-	// GOPATH. We note that fn.Name() returns the function name qualified by
-	// the import path, which does not include the GOPATH. Thus we can trim
-	// segments from the beginning of the file path until the number of path
-	// separators remaining is one more than the number of path separators in
-	// the function name. For example, given:
-	//
-	//    GOPATH     /home/user
-	//    file       /home/user/src/pkg/sub/file.go
-	//    fn.Name()  pkg/sub.Type.Method
-	//
-	// We want to produce:
-	//
-	//    pkg/sub/file.go
-	//
-	// From this we can easily see that fn.Name() has one less path separator
-	// than our desired output. We count separators from the end of the file
-	// path until it finds two more than in the function name and then move
-	// one character forward to preserve the initial path segment without a
-	// leading separator.
-	const sep = "/"
-	goal := strings.Count(fn.Name(), sep) + 2
-	file, line := fn.FileLine(pc)
-	i := len(file)
-	for n := 0; n < goal; n++ {
-		i = strings.LastIndex(file[:i], sep)
-		if i == -1 {
-			// not enough separators found, set i so that the slice expression
-			// below leaves file unmodified
-			i = -len(sep)
-			break
-		}
-	}
-	// get back to 0 or trim the leading separator
-	file = file[i+len(sep):]
-	return file, line
 }
