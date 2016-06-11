9a4f977a0570cd26a97bdab0f50df0004983bb42
Dave Cheney <dave@cheney.net>
2016-06-11 23:12:34 +1000

    document extended format

--- errors.go
+++ errors.go
@@ -43,6 +43,17 @@
 //             // unknown error
 //     }
 //
+// Formatted printing of errors
+//
+// All error values returned from this package implement fmt.Formatter and can
+// be formatted by the fmt package. The following verbs are supported
+//
+//     %s    print the error. If the error has a Cause it will be
+//           printed recursively
+//     %v    see %s
+//     %+v   extended format. Each Frame of the error's Stacktrace will
+//           be printed in detail.
+//
 // Retrieving the stack trace of an error or wrapper
 //
 // New, Errorf, Wrap, and Wrapf record a stack trace at the point they are
