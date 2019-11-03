55262813e432b8f9cbd2058fb7fd3b93af46e0a8
Sherlock Holo <sherlockya@gmail.com>
2019-11-04 00:56:32 +0800

    style: delete useless comments
    Signed-off-by: Sherlock Holo <sherlockya@gmail.com>

--- goold.go
+++ goold.go
@@ -7,19 +7,13 @@ import (
 )
 
 func Is(err, target error) bool {
-	// std errors use internal reflect to optimize performance,
-	// we can we use it.
 	return xerrors.Is(er, target)
 }
 
 func As(err error, target interface{}) bool {
-	// std errors use internal reflect to optimize performance,
-	// we can we use it.
 	return xerrors.As(err, target)
 }
 
 func Unwrap(err error) error {
-	// std errors use internal reflect to optimize performance,
-	// we can we use it.
 	return xerrors.Unwrap(err)
 }
