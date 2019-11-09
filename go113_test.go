afdfa07cbc5f45147ee8378b9b57e58dbeb0f6c5
Jay Petacat <jay@jayschwa.net>
2019-11-09 04:38:58 -0500

    Move go113_test back into errors package

--- go113_test.go
+++ go113_test.go
@@ -1,17 +1,15 @@
 // +build go1.13
 
-package errors_test
+package errors
 
 import (
 	stdlib_errors "errors"
 	"testing"
-
-	"github.com/pkg/errors"
 )
 
 func TestErrorChainCompat(t *testing.T) {
 	err := stdlib_errors.New("error that gets wrapped")
-	wrapped := errors.Wrap(err, "wrapped up")
+	wrapped := Wrap(err, "wrapped up")
 	if !stdlib_errors.Is(wrapped, err) {
 		t.Errorf("Wrap does not support Go 1.13 error chains")
 	}
